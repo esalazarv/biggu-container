c6517b886db4113cedd60f6457469771c21cd8e2
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-04-06 18:38:40 -0500

    Add Base Container

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name = 'biggu-container',
+    packages = ['biggu-container'],
+    version = '0.0.1',
+    package_dir={'':'src'},
+    exclude=["test"],
+    description = 'IoC for python projects',
+    author = 'Eduardo Salazar',
+    author_email = 'eduardosalazar89@hotmail.es',
+    url = 'https://github.com/esalazarv/biggu-container.git',
+    download_url = 'https://github.com/esalazarv/biggu-container.git/tarball/0.0.1',
+    keywords = ['container', 'IoC', 'dependencies', 'injection', 'resolution'],
+    license="MIT",
+    classifiers = [
+        "Programming Language :: Python :: 3.8",
+    ],
+    include_package_data=True,
+    install_requires=[],
+)
