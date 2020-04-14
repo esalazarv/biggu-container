5c8b8d5ce13f332afe697c9d27292e8722a92334
Eduardo Salazar <eduardosalazar89@hotmail.es>
2020-04-14 15:34:48 -0500

    Fix register singleton by Class
    add esure key as string method

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name = 'biggu_container',
     packages = ['biggu_container'],
-    version = 'v0.0.1',
+    version = 'v0.0.2',
     exclude=["test"],
     description = 'IoC for python projects',
     author = 'Eduardo Salazar',
     author_email = 'eduardosalazar89@hotmail.es',
     url = 'https://github.com/esalazarv/biggu-container.git',
-    download_url = 'https://github.com/esalazarv/biggu-container/archive/v0.0.1.zip',
+    download_url = 'https://github.com/esalazarv/biggu-container/archive/v0.0.2.zip',
     keywords = ['biggu_container', 'IoC', 'dependencies', 'injection', 'resolution'],
     license="MIT",
     classifiers = [
